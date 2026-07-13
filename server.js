3e54fcd3ad94ed14a2e7e1158acf35da4b922a09
kelvingabrielh-ai <kelvingabrielh@gmail.com>
2026-07-13 07:09:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,9 +37,6 @@ res.send(`
 <link rel="icon" type="image/png" href="/icon.png?v=2">
 <link rel="icon" type="image/png" href="/favicon.png">
 
-<p class="forgot">
-    ¿Olvidaste tu contraseña?
-</p>
 <style>
 
 *{
